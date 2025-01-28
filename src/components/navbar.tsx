dd2e847b22836646d21686dc6a76a6d71ff82771
Gursharan Singh <gursharansingh4312@gmail.com>
2025-01-27 19:03:00 -0800

    fix faqs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,11 +4,12 @@ import Link from 'next/link'
 
 const navbar = () => {
   const navItems = [
-    { className: "rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white", route: "/", label: "HOME" },
-    { className: "rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white", route: "/accomodations", label: "ACCOMODATION" },
-    { className: "rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white", route: "/transportation", label: "TRANSPORTATION" },
-    { className: "rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white", route: "tours", label: "TOURS" },
-    { className: "rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white", route: "/contact", label: "CONTACT" }
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "/", label: "HOME" },
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "/accomodations", label: "ACCOMODATION" },
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "/transportation", label: "TRANSPORTATION" },
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "tours", label: "TOURS" },
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "/contact", label: "CONTACT" },
+    { className: "rounded-md px-3 py-2 text-sm font-bold text-gray-800 uppercase hover:bg-gray-700 hover:text-white", route: "/faq", label: "FAQs" }
   ]
   return (
     <nav className="w-100 my-10">
@@ -30,7 +31,6 @@ const navbar = () => {
           </div>
           <div className="flex flex-1 flex-col items-center justify-center items-center gap-4 w-100 relative" >
             <div className='flex align-center justify-center w-6/12 relative'>
-              <Link href='/faq' className='absolute left-0 rounded-md px-3 py-2 text-sm font-medium text-gray-500 uppercase hover:bg-gray-700 hover:text-white' >FAQs</Link>
               <Image className="h-8 w-auto" src='/logo.png' width={100} height={20} alt='logo' />
             </div>
             <div className="hidden sm:block">
@@ -39,14 +39,6 @@ const navbar = () => {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
         </div>
       </div>
     </nav>
